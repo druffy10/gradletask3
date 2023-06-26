b3877f1fa97c1c042c017fd34b1ad2fec9f20304
d1sam <d1sam.dot@gmail.com>
2023-06-26 16:53:10 +0300

    fix commit

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ class PostmanEchoTest {
         // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("perfect"))
         ;
     }
 }
