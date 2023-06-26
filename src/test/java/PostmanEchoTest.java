cd031164afad53d8559388327ddb04db4df500cd
d1sam <d1sam.dot@gmail.com>
2023-06-26 16:38:24 +0300

    first commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData(){
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("perfect") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        // Выполняемые действия
+                .when()
+                .post("/post")
+        // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
